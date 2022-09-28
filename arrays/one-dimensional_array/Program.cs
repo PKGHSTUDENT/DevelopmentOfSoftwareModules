727de896adc160d49e22bd6334b3f95c0a0a50a0
dobrodelete <kirkon3004@gmail.com>
2022-09-28 03:48:26 +0300

    arrays

---
+++ arrays/one-dimensional_array/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace MyArray
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Enter n: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int[] arr = new int[n];
+            Random rand = new Random();
+
+            for (int i = 0; i < n; i++)
+                arr[i] = rand.Next(-50, 50);
+
+            foreach (int item in arr) Console.Write($"{item} | ");
+            
+            Console.WriteLine($"\nNumber of elements in the array: {arr.Length}");
+
+            int positive = 0, even = 0;
+
+            foreach (int item in arr)
+            {
+                positive += item > 0 ? item : 0;
+                even += (item % 2 == 0) ? 1 : 0;
+            }
+
+            Console.Write($"Summa positive numbers: {positive} | Even numbers: {even}");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ arrays/two-dimensional array/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace MyArray
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Enter columns: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            
+            Console.Write("Enter rows: ");
+            int m = Convert.ToInt32(Console.ReadLine());
+
+            int[,] arr = new int[n, m];
+
+            Random rand = new Random();
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    arr[i, j] = rand.Next(-50, 50);
+                }
+            }
+
+            /*foreach (int item in arr)
+            {
+                Console.Write(item + " ");
+            }*/
+
+            int max = arr[0, 0];
+            int x = 0, y = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine();
+                for (int j = 0; j < m; j++)
+                {
+                    Console.Write($"{arr[i, j], 3} | ");
+                    if (max < arr[i, j])
+                    {
+                        max = arr[i, j];
+                        x = i;
+                        y = j;
+                    }
+                }
+            }
+
+            Console.Write($"\nMax = {max} | row: {x + 1} | column: {y + 1}");
+
+            Console.ReadKey();
+        }
+    }
+}
